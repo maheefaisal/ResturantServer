a7175084e9f1021c08456cf9a97d60d820086a20
Ehfaz Mahee <mahee504@gmail.com>
2023-08-30 06:02:44 +0600

    added menu data getting code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.3igul5k.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3igul5k.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,21 +26,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("resturantDB").collection("menu")
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray()
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Resturant Server Is Onnn...')
+  res.send('Resturant Server Is Onnn...')
 })
 
 app.listen(port, () => {
-    console.log('This is from the other side')
+  console.log('This is from the other side')
 })
