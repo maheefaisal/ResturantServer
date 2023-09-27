7fa77a1ceb61acc5d875f03f8ef6ec24407bc4e5
Ehfaz Mahee <mahee504@gmail.com>
2023-09-27 18:49:36 +0600

    made changes to index file

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ async function run() {
       const result = await menuCollection.find().toArray()
       res.send(result);
     })
-    app.post('/menu', async (req, res) => {
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
       const newItem = req.body
       const result = await menuCollection.insertOne(newItem)
       res.send(result)
